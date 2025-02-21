fdd21e524e176aaf18bb66cd31c54f4e0ef54937
Shivraj Gawali <65460414+3n881@users.noreply.github.com>
2025-02-21 23:02:56 +0530

    Update main.py

--- src/travel_agent_ai/main.py
+++ src/travel_agent_ai/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import sys
-import warnings
+# import warnings
 
 from datetime import datetime
 
 from travel_agent_ai.crew import TravelAgentAi
 
-warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+# warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
@@ -37,5 +37,4 @@ def run():
     }
     TravelAgentAi().crew().kickoff(inputs=inputs)
 
-if __name__ == "__main__":
-    run()
+
