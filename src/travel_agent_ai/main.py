d99514165520e7a2fab2851bb6bc8304d75e31a5
Shivraj Gawali <65460414+3n881@users.noreply.github.com>
2025-02-21 22:36:38 +0530

    Update main.py

--- src/travel_agent_ai/main.py
+++ src/travel_agent_ai/main.py
@@ -17,30 +17,25 @@ def run():
     """
     Run the crew with either interactive user input or webhook inputs.
     """
-    
-    
     inputs = {
-            "source_location": "New York, USA",
-            "destination_location": "Paris, France",
-            "travel_date": "2025-03-15",
-            "travel_duration": "7 days",
-            "number_of_travelers": 2,
-            "travel_companions_type": "Family",
-            "budget_per_person": 1500,
-            "preferred_travel_mode": "Flight",
-            "accommodation_type": "Hotel",
-            "meal_preferences": "Vegetarian",
-            "interests": ["Museums", "Historical Sites", "Food Tasting"],
-            "accessibility_needs": "Wheelchair accessible hotel room",
-            "dietary_restrictions": "Gluten-free",
-            "special_requests": "Early check-in and city view room",
-            
-            'date': datetime.now().isoformat()
-        
+            "source_location": "<New York, USA>",
+            "destination_location": "<Paris, France>",
+            "travel_date": "<2025-03-15>",
+            "travel_duration": "<7 days>",
+            "number_of_travelers": "<trevelers>",
+            "travel_companions_type": "<Family>",
+            "budget_per_person": "<1500>",
+            "preferred_travel_mode": "<Flight>",
+            "accommodation_type": "<Hotel>",
+            "meal_preferences": "<Vegetarian>",
+            "interests": "<Museums>",
+            "accessibility_needs": "<Wheelchair accessible hotel room>",
+            "dietary_restrictions": "<Gluten-free>",
+            "special_requests": "<Early check-in and city view room>",
+            "date":"<date>",
+            # "date": datetime.now().isoformat(),
     }
-    
-    result = TravelAgentAi().crew().kickoff(inputs=inputs)
-    print(result)
+    TravelAgentAi().crew().kickoff(inputs=inputs)
 
 if __name__ == "__main__":
     run()
