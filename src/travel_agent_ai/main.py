0b345f6d56c3185f25c4483b2aeb0684bb609849
Shivraj Gawali <65460414+3n881@users.noreply.github.com>
2025-02-21 23:17:52 +0530

    Update main.py

--- src/travel_agent_ai/main.py
+++ src/travel_agent_ai/main.py
@@ -15,26 +15,65 @@ from travel_agent_ai.crew import TravelAgentAi
 
 def run():
     """
-    Run the crew with either interactive user input or webhook inputs.
+    Run the crew.
     """
     inputs = {
-            "source_location": "<New York, USA>",
-            "destination_location": "<Paris, France>",
-            "travel_date": "<2025-03-15>",
-            "travel_duration": "<7 days>",
-            "number_of_travelers": "<trevelers>",
-            "travel_companions_type": "<Family>",
-            "budget_per_person": "<1500>",
-            "preferred_travel_mode": "<Flight>",
-            "accommodation_type": "<Hotel>",
-            "meal_preferences": "<Vegetarian>",
-            "interests": "<Museums>",
-            "accessibility_needs": "<Wheelchair accessible hotel room>",
-            "dietary_restrictions": "<Gluten-free>",
-            "special_requests": "<Early check-in and city view room>",
-            "date":"<date>",
-            # "date": datetime.now().isoformat(),
+            "source_location": "New York, USA",
+            "destination_location": "Paris, France",
+            "travel_date": "2025-03-15",
+            "travel_duration": "7 days",
+            "number_of_travelers": 2,
+            "travel_companions_type": "Family",
+            "budget_per_person": 1500,
+            "preferred_travel_mode": "Flight",
+            "accommodation_type": "Hotel",
+            "meal_preferences": "Vegetarian",
+            "interests": ["Museums", "Historical Sites", "Food Tasting"],
+            "accessibility_needs": "Wheelchair accessible hotel room",
+            "dietary_restrictions": "Gluten-free",
+            "special_requests": "Early check-in and city view room",
+            "date": datetime.now().isoformat(),
     }
     TravelAgentAi().crew().kickoff(inputs=inputs)
 
+# if __name__ == "__main__":
+#     run()
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {"topic": "AI LLMs"}
+    try:
+        TravelAgentAi().crew().train(
+            n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs
+        )
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        TravelAgentAi().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {"topic": "AI LLMs"}
+    try:
+        TravelAgentAi().crew().test(
+            n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs
+        )
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
 
